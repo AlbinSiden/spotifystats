350a03c6f7791ce074e51af4f5e1ab51f03a975a
bappoDev <83096191+bappoDev@users.noreply.github.com>
2021-04-23 23:30:50 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// I wanted to use store but on path change store cleared? Decided on localstorage instead because the spotify api token isnt valuable.
+// https://forum.vuejs.org/t/vuex-state-data-change-after-route-change/6562
+
 export default new Vuex.Store({
   state: {
     user: null,
